219cc53e92d3df05919ab906570259ef7a6d70ac
shalindra shrestha <shalindra.shrestha@gmail.com>
2018-07-24 19:11:04 -0700

    medias,  contactMe and 404 Page
    medias,  contactMe and 404 Page

---
+++ imports/ui/Lost.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../public/LostPage.css'
+
+export default function Lost(){
+    return (
+        <div className="lostPage">
+            <div className="c">
+                <div className="_404">404</div>
+                <hr />
+                <div className="_1">THE PAGE</div>
+                <div className="_2">WAS NOT FOUND</div>
+                <a className="btn" href="/">BACK TO SOCCER</a>
+            </div>
+            </div>
+
+    )
+}
